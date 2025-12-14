8edd6f7b8f6137a3f9debd959bbf424e5d8c4fa4
abhijeet choudhury <abhijeetchoudhury@Abhijeets-MacBook-Air.local>
2025-12-15 03:24:36 +0530

    commented out main fn in seed-ts

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -190,11 +190,11 @@ async function main() {
   console.log('Sample appointment id:', appointment.id);
 }
 
-main()
-  .catch((e) => {
-    console.error(e);
-    process.exit(1);
-  })
-  .finally(async () => {
-    await prisma.$disconnect();
-  });
+// main()
+//   .catch((e) => {
+//     console.error(e);
+//     process.exit(1);
+//   })
+//   .finally(async () => {
+//     await prisma.$disconnect();
+//   });
